dee7f1dd22a9090a4050601f379c0b2a8856f78f
Nguyễn Đăng Cường <dangcuong201004@gmail.com>
2025-04-07 06:27:07 +0700

    first commit

---
+++ mvc/views/ListProductsView.php
@@ -0,0 +1,211 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop - GearBK Store</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../../public/css/product.css">
+    <link rel="stylesheet" href="../../public/css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="inner-content">
+                        <div class="inner-logo">
+                            <img src="../../public/images/LogoGearBK.webp" alt="">
+                            <span>GearBK</span>
+                        </div>
+                        <div class="inner-menu">
+                            <ul>
+                                <li>
+                                    <a href="">HOME</a>
+                                </li>
+                                <li>
+                                    <a href="">ABOUT</a>
+                                </li>
+                                <li>
+                                    <a href="">SHOP</a>
+                                </li>
+                                <li>
+                                    <a href="">CONTACT</a>
+                                </li>
+                                <li>
+                                    <a href="">NEWS</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="inner-shop"><i class="fa-solid fa-bag-shopping"></i></div>
+                        <div class="inner-user"><i class="fa-solid fa-user"></i></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="banner">
+        <img src="../../public/images/Banner-demo.jpg" alt="">
+    </div>
+
+    <div class="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <div class="inner-filter inner-left">
+                        <div class="inner-brand">
+                            <form action="#" form-check-brand>
+                                <div class="inner-title">Thương hiệu</div>
+                                <div class="row">
+                                    <div class="col-6"><input type="checkbox" id="all" name="all" value="all">
+                                        <label for="all">Tất cả</label>
+                                    </div>
+                                    <div class="col-6">
+                                        <input type="checkbox" id="Xgear" name="Xgear" value="xgear">
+                                        <label for="Xgear">Xgear</label> <br>
+                                    </div>
+                                    <div class="col-6">
+                                        <input type="checkbox" id="Gigabye" name="Gigabye" value="gigabye">
+                                        <label for="Gigabye">Gigabye</label>
+                                    </div>
+                                    <div class="col-6">
+                                        <input type="checkbox" id="MSI" name="MSI" value="msi">
+                                        <label for="MSI">MSI</label> <br>
+                                    </div>
+                                    <div class="col-6">
+                                        <input type="checkbox" id="Asus" name="Asus" value="asus">
+                                        <label for="Asus">Asus</label>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div class="inner-price">
+                            <form action="#" form-check-price>
+                                <div class="inner-title">Lọc giá</div>
+                                <div class="row">
+                                    <div class="col-12"><input type="checkbox" id="all" name="all" value="all">
+                                        <label for="all">Tất cả</label>
+                                    </div>
+                                    <div class="col-12">
+                                        <input type="checkbox" id="duoi-500" name="duoi-500" value="duoi-500">
+                                        <label for="duoi-500">Dưới 500,000₫</label> <br>
+                                    </div>
+                                    <div class="col-12">
+                                        <input type="checkbox" id="500-5000" name="500-5000" value="500-5000">
+                                        <label for="500-5000">500,000₫ - 5,000,000₫</label>
+                                    </div>
+                                    <div class="col-12">
+                                        <input type="checkbox" id="5000-15000" name="5000-15000" value="5000-15000">
+                                        <label for="5000-15000">5,000,000₫ - 15,000,000₫</label> <br>
+                                    </div>
+                                    <div class="col-12">
+                                        <input type="checkbox" id="15000-30000" name="15000-30000" value="15000-30000">
+                                        <label for="15000-30000">15,000,000₫ - 30,000,000₫</label>
+                                    </div>
+                                    <div class="col-12">
+                                        <input type="checkbox" id="tren-30000" name="tren-30000" value="tren-30000">
+                                        <label for="tren-30000">Trên 30,000,000₫</label>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-9 inner-right">
+                    <div class="inner-search">
+                        <input type="text" placeholder="Khám phá sản phẩm tại GearBK"> <i class="fa-solid fa-magnifying-glass"></i>
+                    </div>
+                    <div class="inner-sort">
+                        <button type="button" class="btn btn-danger">Nổi bật</button>
+                        <button type="button" class="btn btn-light">Giá tăng dần</button>
+                        <button type="button" class="btn btn-light">Giá giảm dần</button>
+                        <button type="button" class="btn btn-light">A-Z</button>
+                        <button type="button" class="btn btn-light">Z-A</button>
+                        <button type="button" class="btn btn-light">Mới nhất</button>
+                        <button type="button" class="btn btn-light">Bán chạy</button>
+                    </div>
+                    <div class="inner-product-list">
+                        <div class="row">
+                        <?php if (mysqli_num_rows($data["listProducts"]) > 0): ?>
+                            <?php while ($row = mysqli_fetch_assoc($data["listProducts"])): ?>
+                                <div class="col-4">
+                                    <a href="detail.php?slug=<?= $row['slug'] ?>">
+                                        <div class="inner-item">
+                                            <img src="../../public/images/<?= $row['images'] ?>" alt="">
+                                            <div class="inner-title"><?= $row['name'] ?></div>
+                                            <span class="inner-new-price"><?= number_format($row['price']) ?>₫</span>
+                                            <?php if ($row['discount'] > 0): ?>
+                                                <span class="inner-old-price">
+                                                    <?= number_format($row['price'] * 100 / (100 - $row['discount'])) ?>₫
+                                                </span>
+                                            <?php endif; ?>
+                                            <div class="inner-more-info">
+                                                <div class="inner-preview">
+                                                    <img src="images/Chip.webp" alt="">
+                                                    <div class="inner-text">Intel Core i3-12100F</div>
+                                                </div>
+                                                <div class="inner-preview">
+                                                    <img src="images/RTX.webp" alt="">
+                                                    <div class="inner-text">RTX 3050</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+              
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <div class="inner-menu">
+                        <ul>
+                            <li>
+                                <a href="">Home</a>
+                            </li>
+                            <li>
+                                <a href="">About</a>
+                            </li>
+                            <li>
+                                <a href="">Shop</a>
+                            </li>
+                            <li>
+                                <a href="">Contact</a>
+                            </li>
+                            <li>
+                                <a href="">News</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="inner-name">
+                        GEARBK STORE
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="inner-conpyright">
+                        Copyright © 2025 GearBK Store
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ sqp.sql
@@ -0,0 +1,107 @@
+CREATE TABLE products (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,                -- Tên sản phẩm
+    code VARCHAR(20),                          -- Mã sản phẩm
+    images LONGTEXT,                           -- Ảnh (có thể JSON chuỗi nhiều ảnh)
+    price INT,                                 -- Giá bán
+    discount INT,                              -- % giảm giá (nếu có)
+    description LONGTEXT,                      -- Mô tả chi tiết
+    specs LONGTEXT,                            -- Cấu hình / thông số kỹ thuật
+    quantity INT,                              -- Số lượng tồn kho
+    status VARCHAR(20),                        -- Trạng thái (còn hàng, ngừng KD,...)
+    slug VARCHAR(255) NOT NULL,                -- Tên SEO (pc-gaming-x305)
+    deleted BOOLEAN DEFAULT FALSE,             -- Xóa mềm
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE categories (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,               -- Tên danh mục
+    image VARCHAR(500),                        -- Ảnh đại diện danh mục
+    description LONGTEXT,                      -- Mô tả danh mục
+    status VARCHAR(20),                        -- Trạng thái (active/inactive)
+    slug VARCHAR(255) NOT NULL,                -- Đường dẫn SEO
+    deleted BOOLEAN DEFAULT FALSE,             -- Xóa mềm
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL,
+    PRIMARY KEY (id)
+);
+
+
+
+CREATE TABLE products_categories (
+    product_id INT,
+    category_id INT,
+    PRIMARY KEY (product_id, category_id),
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+
+
+CREATE TABLE orders (
+    id INT NOT NULL AUTO_INCREMENT,
+    code VARCHAR(10) NOT NULL,                -- Mã đơn hàng (vd: ORD12345)
+    full_name VARCHAR(50) NOT NULL,           -- Tên người nhận
+    phone VARCHAR(10) NOT NULL,               -- SĐT người nhận
+    note VARCHAR(500),                        -- Ghi chú đơn hàng
+    status VARCHAR(20),                       -- Trạng thái (pending, confirmed,...)
+    deleted BOOLEAN DEFAULT FALSE,            -- Xóa mềm
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE orders_products (
+    id INT NOT NULL AUTO_INCREMENT,
+    order_id INT NOT NULL,                     -- Mã đơn hàng
+    product_id INT NOT NULL,                   -- Mã sản phẩm
+    quantity INT NOT NULL,                     -- Số lượng mua
+    price INT NOT NULL,                        -- Giá tại thời điểm mua
+    discount INT,                              -- Giảm giá (nếu có)
+    PRIMARY KEY (id),
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+
+-- categories
+INSERT INTO categories (title, image, description, status, slug) VALUES ('PC Gaming', 'pc-gaming.jpg', 'Máy tính chơi game hiệu năng cao', 'active', 'pc-gaming');
+INSERT INTO categories (title, image, description, status, slug) VALUES ('Laptop Văn Phòng', 'laptop-vanphong.jpg', 'Laptop mỏng nhẹ cho công việc', 'active', 'laptop-van-phong');
+INSERT INTO categories (title, image, description, status, slug) VALUES ('Màn Hình', 'man-hinh.jpg', 'Màn hình Full HD, 2K, 4K', 'active', 'man-hinh');
+INSERT INTO categories (title, image, description, status, slug) VALUES ('Phụ Kiện', 'phu-kien.jpg', 'Chuột, bàn phím, tai nghe...', 'active', 'phu-kien');
+INSERT INTO categories (title, image, description, status, slug) VALUES ('Thiết Bị Mạng', 'network.jpg', 'Router, Switch, WiFi Mesh', 'active', 'thiet-bi-mang');
+
+-- products
+INSERT INTO products (name, code, images, price, discount, description, quantity, status, slug) VALUES ('PC Gaming X305', 'SP0001', 'pc-gaming-x305.jpg', 9361678, 5, 'Sản phẩm PC Gaming X305 phù hợp với nhu cầu sử dụng cá nhân hoặc doanh nghiệp.', 47, 'active', 'pc-gaming-x305');
+INSERT INTO products (name, code, images, price, discount, description, quantity, status, slug) VALUES ('Laptop HP G3', 'SP0002', 'laptop-hp-g3.jpg', 17389795, 0, 'Sản phẩm Laptop HP G3 phù hợp với nhu cầu sử dụng cá nhân hoặc doanh nghiệp.', 36, 'active', 'laptop-hp-g3');
+INSERT INTO products (name, code, images, price, discount, description, quantity, status, slug) VALUES ('Màn Hình LG 24inch', 'SP0003', 'màn-hình-lg-24inch.jpg', 8348463, 10, 'Sản phẩm Màn Hình LG 24inch phù hợp với nhu cầu sử dụng cá nhân hoặc doanh nghiệp.', 62, 'active', 'màn-hình-lg-24inch');
+INSERT INTO products (name, code, images, price, discount, description, quantity, status, slug) VALUES ('Chuột Logitech G102', 'SP0004', 'chuột-logitech-g102.jpg', 11188129, 10, 'Sản phẩm Chuột Logitech G102 phù hợp với nhu cầu sử dụng cá nhân hoặc doanh nghiệp.', 66, 'active', 'chuột-logitech-g102');
+INSERT INTO products (name, code, images, price, discount, description, quantity, status, slug) VALUES ('Router WiFi TP-Link', 'SP0005', 'router-wifi-tp-link.jpg', 11389895, 5, 'Sản phẩm Router WiFi TP-Link phù hợp với nhu cầu sử dụng cá nhân hoặc doanh nghiệp.', 75, 'active', 'router-wifi-tp-link');
+INSERT INTO products (name, code, images, price, discount, description, quantity, status, slug) VALUES ('PC Gaming AMD', 'SP0006', 'pc-gaming-amd.jpg', 11893045, 10, 'Sản phẩm PC Gaming AMD phù hợp với nhu cầu sử dụng cá nhân hoặc doanh nghiệp.', 17, 'active', 'pc-gaming-amd');
+INSERT INTO products (name, code, images, price, discount, description, quantity, status, slug) VALUES ('Laptop Asus Zenbook', 'SP0007', 'laptop-asus-zenbook.jpg', 18833524, 10, 'Sản phẩm Laptop Asus Zenbook phù hợp với nhu cầu sử dụng cá nhân hoặc doanh nghiệp.', 38, 'active', 'laptop-asus-zenbook');
+INSERT INTO products (name, code, images, price, discount, description, quantity, status, slug) VALUES ('Màn hình Dell 27inch', 'SP0008', 'màn-hình-dell-27inch.jpg', 12032111, 10, 'Sản phẩm Màn hình Dell 27inch phù hợp với nhu cầu sử dụng cá nhân hoặc doanh nghiệp.', 99, 'active', 'màn-hình-dell-27inch');
+INSERT INTO products (name, code, images, price, discount, description, quantity, status, slug) VALUES ('Bàn phím cơ Razer', 'SP0009', 'bàn-phím-cơ-razer.jpg', 7159294, 10, 'Sản phẩm Bàn phím cơ Razer phù hợp với nhu cầu sử dụng cá nhân hoặc doanh nghiệp.', 35, 'active', 'bàn-phím-cơ-razer');
+INSERT INTO products (name, code, images, price, discount, description, quantity, status, slug) VALUES ('Switch 8 cổng', 'SP0010', 'switch-8-cổng.jpg', 7780563, 5, 'Sản phẩm Switch 8 cổng phù hợp với nhu cầu sử dụng cá nhân hoặc doanh nghiệp.', 20, 'active', 'switch-8-cổng');
+
+
+INSERT INTO products_categories (product_id, category_id) VALUES (1, 1);
+INSERT INTO products_categories (product_id, category_id) VALUES (2, 2);
+INSERT INTO products_categories (product_id, category_id) VALUES (3, 3);
+INSERT INTO products_categories (product_id, category_id) VALUES (4, 4);
+INSERT INTO products_categories (product_id, category_id) VALUES (5, 5);
+INSERT INTO products_categories (product_id, category_id) VALUES (6, 1);
+INSERT INTO products_categories (product_id, category_id) VALUES (7, 2);
+INSERT INTO products_categories (product_id, category_id) VALUES (8, 3);
+INSERT INTO products_categories (product_id, category_id) VALUES (9, 4);
+INSERT INTO products_categories (product_id, category_id) VALUES (10, 5);
+
+
+
